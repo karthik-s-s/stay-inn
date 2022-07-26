2c1a1005591fa392d08ec38bd8c5871abf1daa5a
karthik sudhakar <karthiksudhakar999@gmail.com>
2022-07-26 15:25:56 +0530

    ready to deploy

---
+++ config/twilio.js
@@ -0,0 +1,9 @@
+// const twilio = require('twilio');
+
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = process.env.TWILIO_AUTH_TOKEN;
+// const client = require('twilio')(accountSid, authToken);
+
+// client.verify.v2.services
+//                 .create({friendlyName: 'My First Verify Service'})
+//                 .then(service => console.log(service.sid));
